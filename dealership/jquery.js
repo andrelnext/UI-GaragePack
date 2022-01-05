57d023a4ee26d44cd6e1c855afffb4cffe200f0f
Andre B C <cristo.andre@escola.pr.gov.br>
2022-01-05 03:46:23 -0300

    Bennys - Carrousel & Pre-Jquery ainda em testes

--- dealership/jquery.js
+++ dealership/jquery.js
@@ -1,18 +1,39 @@
-$(document).ready(function () {
-    $('.menu-option-car').hover(function (e) {
-            e.preventDefault();
-            $(this).addClass('active img price');
-            $(this).removeClass("align");
-            $('.price').css('display', 'flex');
 
-        }, function () {
-            $(this).removeClass("active img price");
-            $(this).addClass("align");
 
-            $('.car-object').remove("img");
-            $('.price').css('display', 'none');
+var carros = [
+    { nome: "Bmw 320i", status: "Preso", img: "../assets/image%2028.png", preco: 50000 },
+    { nome: "Bmw M4", status: "Liberado", img: "asdf", preco: 450000 },
+    { nome: "Bmw M4", status: "Liberado", img: "asdf", preco: 450000 },
+    { nome: "Bmw M4", status: "Liberado", img: "asdf", preco: 450000 },
+]
 
-            $('img').css('display', 'flex');
+function setCarros(carros) {
+    $.each(carros, function (index, data) {
+        $("#menu-cars").append(`
+      <div class="menu-option-car align">
+        <div class="light-text"> ${data.status} <div class="light green"></div>
+        <img src="${data.img}" alt="" class="icon-status">
+        <div class="price"><p>${data.preco}</p></div>
+        <div class="typecar">${data.nome}</div>
+      </div>
+    `);
+    });
+}
+
+function myFunction() {
+    let input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("menu-cars");
+    li = ul.getElementById('menu-option-car');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("p")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
         }
-    );
-});
+    }
+}
